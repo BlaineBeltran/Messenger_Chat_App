aa488296bb5cecb51f529220c583c63c0122144b
Blaine <blaine.abeltran@gmail.com>
2020-11-05 10:45:01 -0600

    Added Text fields for login page

---
+++ Messenger_Chat_App/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  Messenger_Chat_App
+//
+//  Created by Blaine Beltran on 11/5/20.
+//  Copyright © 2020 Blaine Beltran. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .link
+    }
+    
+
+
+}
