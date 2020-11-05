5a1562966e2de2dca36bca3bace5f2de2074fb5b
Blaine <blaine.abeltran@gmail.com>
2020-11-05 09:17:26 -0600

    Initial Commit

---
+++ Messenger_Chat_AppTests/Messenger_Chat_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Messenger_Chat_AppTests.swift
+//  Messenger_Chat_AppTests
+//
+//  Created by Blaine Beltran on 11/5/20.
+//  Copyright © 2020 Blaine Beltran. All rights reserved.
+//
+
+import XCTest
+@testable import Messenger_Chat_App
+
+class Messenger_Chat_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
