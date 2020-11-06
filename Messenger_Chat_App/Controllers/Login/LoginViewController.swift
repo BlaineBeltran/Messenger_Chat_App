31e1572359cf0e390b4f804250aa77501406bd87
Blaine <blaine.abeltran@gmail.com>
2020-11-06 10:36:14 -0600

    added firebase authentication via email

--- Messenger_Chat_App/Controllers/Login/LoginViewController.swift
+++ Messenger_Chat_App/Controllers/Login/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
@@ -114,6 +115,15 @@ class LoginViewController: UIViewController {
         }
         
         // Firebase login
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { authResult, error in
+            guard let result = authResult, error == nil else {
+                print("Failed to login user with email")
+                return
+            }
+            
+            let user = result.user
+            print("Logged in user \(user)")
+        })
     }
     
     func alertUserLoginError() {
--- Messenger_Chat_App/Controllers/Login/RegisterViewController.swift
+++ Messenger_Chat_App/Controllers/Login/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
     
@@ -169,6 +170,15 @@ class RegisterViewController: UIViewController {
         }
         
         // Firebase login
+        
+        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: { authResult, error in
+            guard let  result = authResult, error == nil else {
+                print("Error creating user")
+                return
+            }
+            let user = result.user
+            print("Created User: \(user)")
+        })
     }
     
     func alertUserLoginError() {
